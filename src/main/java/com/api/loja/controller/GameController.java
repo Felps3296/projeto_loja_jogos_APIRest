698dbf87d1cc320c07141eb777bb06d60defd792
unknown <felipereis.live@gmail.com>
2024-11-05 19:53:03 -0300

    Corrgindo controller metodos

--- src/main/java/com/api/loja/controller/GameController.java
+++ src/main/java/com/api/loja/controller/GameController.java
@@ -1,14 +1,17 @@
 package com.api.loja.controller;
 
+import com.api.loja.game.Game;
 import com.api.loja.game.GameDAO;
 import com.api.loja.game.RegisterGame;
+import com.api.loja.game.UpdateGame;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/games")
@@ -27,5 +30,27 @@ public class GameController {
         gameDAO.save(game);
         return ResponseEntity.status(HttpStatus.CREATED).body("Game saved successfully");
     }
+
+    @GetMapping
+    public ResponseEntity<List<Game>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable paginacao) {
+
+        List<Game> gamesPage = gameDAO.findAll(paginacao);
+        return ResponseEntity.ok(gamesPage);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> update(@PathVariable Long id, @RequestBody UpdateGame game){
+
+        gameDAO.update(id, game);
+        return ResponseEntity.status(HttpStatus.OK).body("Game updated successfully");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+
+        gameDAO.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
 
