c97e129d6783a286c1bc8a1590f81856e65722b1
unknown <felipereis.live@gmail.com>
2024-11-06 19:23:12 -0300

    Ajustando crud game

--- src/main/java/com/api/loja/game/GameRepository.java
+++ src/main/java/com/api/loja/game/GameRepository.java
@@ -1,13 +1,13 @@
 package com.api.loja.game;
 
 import org.springframework.data.domain.Pageable;
-
 import java.util.List;
+import java.util.Optional;
 
 public interface GameRepository {
 
     void save(RegisterGame game);
-    List<Game> findAll(Pageable paginacao);
     void update(Long id, UpdateGame game);
-    void delete(Long id);
+    void delete(Long id);;
+    List<Game> findAll(Pageable paginacao);
 }
--- src/main/java/com/api/loja/game/UpdateGame.java
+++ src/main/java/com/api/loja/game/UpdateGame.java
@@ -2,6 +2,6 @@ package com.api.loja.game;
 
 import java.math.BigDecimal;
 
-public record UpdateGame(String title, String genre, String releaseDate, String ageRating, BigDecimal price, int stock_quantity ) {
+public record UpdateGame(Long id, String title, String genre, String releaseDate, String ageRating, BigDecimal price, int stock_quantity ) {
 
 }
